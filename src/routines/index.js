04612ab5a9cff98ca052f3a9ae8101eece190586
Vladimir Bobrov <ceo.bobrov@yandex.ru>
2021-05-30 00:57:50 +0700

    Stop clean

--- src/routines/index.js
+++ src/routines/index.js
@@ -105,7 +105,7 @@ const startRoutine = async () => {
 
   startHandleBlockRoutine();
 
-  startCleanTransactionsLastHourCollection();
+  //startCleanTransactionsLastHourCollection();
 
   startCalculateRewardsPerDay();
 
